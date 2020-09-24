51b6348f7bc2691937da1c6db02418cf36725925
maagjoel <42876759+maagjoel@users.noreply.github.com>
2020-09-24 10:12:20 -0400

    Add files via upload
     Updated so user can now log in successfully. Need to complete the different commands now.

--- ftpClient.java
+++ ftpClient.java
@@ -1,24 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 public class ftpClient {
 
-	private static final String SERVER_IP = "inet.cs.fiu.edu";
-	private static final int SERVER_PORT = 9090;
+	private static final int SERVER_PORT = 21;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String SERVER_IP = args[0];
+		
+		
+		/*System.out.println("Enter FTP username: ");
+		String username = scan.nextLine();
+		System.out.println("Enter FTP password: ");
+		String password = scan.nextLine();
+		 */
+
 		Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+		
+		//''DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 		
 		BufferedReader input = new BufferedReader (new InputStreamReader( socket.getInputStream()));
 		BufferedReader keyboard = new BufferedReader (new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 		
+		String serverResponse = input.readLine();
+		JOptionPane.showMessageDialog(null,serverResponse);
 		
+		System.out.println("Enter FTP username: ");
+		String username = "user " + keyboard.readLine();
+		out.println(username);
+		serverResponse = input.readLine();
+		System.out.println(serverResponse);
+		
+		System.out.println("Enter FTP password: ");
+		String password = "pass " + keyboard.readLine();
+		out.println(password);
+		serverResponse = input.readLine();
+		System.out.println(serverResponse);
 		
 		while(true) {
 			
@@ -28,11 +50,11 @@ public class ftpClient {
 		
 		
 		if(command.equals("quit")) break;
-				
+		
 		out.println(command);
 		
-		String serverResponse = input.readLine();
-		JOptionPane.showMessageDialog(null,serverResponse);
+		serverResponse = input.readLine();
+		System.out.println(serverResponse);
 		}
 		
 		socket.close();
