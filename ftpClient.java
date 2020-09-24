5c999f7379480b18d04e2fbb84ba9506303358c3
maagjoel <42876759+maagjoel@users.noreply.github.com>
2020-09-24 18:31:50 -0400

    Add files via upload
    All commands seem to work. We need to find a way to print out the received directories from ls. Everything else works. However, when we use a passive command and want to go use a command that is not passive, the program seems to crash. We are not using the socket with the remote port and we suspect that may have to do something with it.  We also changed the input so it could match the project guidelines.

--- ftpClient.java
+++ ftpClient.java
@@ -3,6 +3,7 @@ import java.net.*;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
+
 public class ftpClient {
 
 	private static final int SERVER_PORT = 21;
@@ -11,54 +12,149 @@ public class ftpClient {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		String SERVER_IP = args[0];
-		
-		
-		/*System.out.println("Enter FTP username: ");
-		String username = scan.nextLine();
-		System.out.println("Enter FTP password: ");
-		String password = scan.nextLine();
+
+		/*
+		 * System.out.println("Enter FTP username: "); String username =
+		 * scan.nextLine(); System.out.println("Enter FTP password: "); String password
+		 * = scan.nextLine();
 		 */
 
 		Socket socket = new Socket(SERVER_IP, SERVER_PORT);
-		
-		//''DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-		
-		BufferedReader input = new BufferedReader (new InputStreamReader( socket.getInputStream()));
-		BufferedReader keyboard = new BufferedReader (new InputStreamReader(System.in));
+
+		// ''DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+		BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
 		
+
 		String serverResponse = input.readLine();
-		JOptionPane.showMessageDialog(null,serverResponse);
-		
+		System.out.println(serverResponse);
+
 		System.out.println("Enter FTP username: ");
 		String username = "user " + keyboard.readLine();
 		out.println(username);
 		serverResponse = input.readLine();
 		System.out.println(serverResponse);
-		
+
 		System.out.println("Enter FTP password: ");
 		String password = "pass " + keyboard.readLine();
 		out.println(password);
 		serverResponse = input.readLine();
 		System.out.println(serverResponse);
-		
-		while(true) {
-			
-		
-		System.out.println("> ");
-		String command = keyboard.readLine();
-		
-		
-		if(command.equals("quit")) break;
-		
-		out.println(command);
-		
+		/*out.println("PASV");
 		serverResponse = input.readLine();
 		System.out.println(serverResponse);
+		int remotePort = getRemotePort(serverResponse); */
+
+		while (true) {
+
+			System.out.println("> ");
+			String command = keyboard.readLine();
+
+			if (command.substring(0,2).equals("ls") || command.substring(0,3).equals("get") || command.substring(0,3).equals("put")) {
+				out.println("PASV");
+				serverResponse = input.readLine();
+				System.out.println(serverResponse);
+				int remotePort = getRemotePort(serverResponse);
+				Socket newSocket = new Socket(SERVER_IP, remotePort);
+				
+				if(command.equals("ls")) {
+					
+					/*out.println("PWD");
+					serverResponse = input.readLine();
+					File folder = new File(serverResponse);
+					File[] listOfFiles = folder.listFiles();
+					System.out.println(listOfFiles.length);
+					for(int i = 0; i < listOfFiles.length; i++) {
+					System.out.println(listOfFiles[i].getName());
+					} */
+					out.println("TYPE A");
+					out.println("LIST");
+					serverResponse = input.readLine();
+					System.out.println(serverResponse);
+					serverResponse = input.readLine();
+					System.out.println(serverResponse);
+					
+					
+				}
+				else if (command.substring(0, 3).equals("get")) {
+					
+					String path = command.substring(4);
+					out.println("RETR " + path);
+					serverResponse = input.readLine();
+					System.out.println(serverResponse);
+
+				} else if (command.substring(0, 3).equals("put")) {
+					
+					String path = command.substring(4);
+					out.println("STOR " + path);
+					serverResponse = input.readLine();
+					System.out.println(serverResponse);
+
+				}
+					
+				
+			} else if (command.substring(0,2).equals("cd")) {
+				
+				String path = command.substring(4);
+				out.println("CWD " + path);
+
+			}  else if (command.equals("delete")) {
+				System.out.println("Enter directory path");
+				String path = command.substring(7);
+				out.println("DELE " + path);
+			}
+
+			else if (command.equals("quit")) {
+				out.println("QUIT");
+				serverResponse = input.readLine();
+				System.out.println(serverResponse);
+				break;
+			}
+				
+
+			//out.println(command);
+
+			serverResponse = input.readLine();
+			System.out.println(serverResponse);
+			
 		}
-		
-		socket.close();
-		System.exit(0);
+
+		//socket.close();
+		 out.close();
+		System.exit(1);
+
+	}
+
+	public static int getRemotePort(String serverResponse) {
+		/* geting the 2 numbers for the client port # on server */
+		String num1 = "";
+		String num2 = "";
+		int commaCounter = 0;
+		for (int i = 0; i < serverResponse.length() - 1; i++) {
+
+			if (commaCounter == 4) {
+				while (serverResponse.charAt(i) != ',') {
+					num1 += serverResponse.charAt(i);
+					i++;
+				}
+				// commaCounter++;
+
+			}
+			if (commaCounter == 5) {
+				while (serverResponse.charAt(i) != ')') {
+					num2 += serverResponse.charAt(i);
+					i++;
+				}
+			}
+
+			if (serverResponse.charAt(i) == ',')
+				commaCounter++;
+		}
+		int result = (Integer.parseInt(num1) * 256) + Integer.parseInt(num2);
+		return result;
 	}
 
 }
